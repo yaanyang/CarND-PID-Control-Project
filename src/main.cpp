171ac432620855a3c3c128ffbfc42444e0fda70f
Chris Yang <Chris.Yang@wdc.com>
2018-06-07 14:16:25 -0700

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main()
   pid_s.Init(0.134611, 0.000270736, 3.05349);
 
   // PID controller for throuttle
-  pid_t.Init(0.1, 0.002, 0.0);
+  pid_t.Init(0.1, 0.0002, 0.0);
 
   h.onMessage([&pid_s, &pid_t](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
