b04cc34dc7de47ab755c14c4068cdcfd0d033d76
Chris Yang <Chris.Yang@wdc.com>
2018-06-22 11:28:09 -0700

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -36,10 +36,10 @@ int main()
   // TODO: Initialize the pid variable.
   
   // PID controller for steering
-  pid_s.Init(0.2, 0.0001, 1.5);
+  pid_s.Init(2.9331, 0.4931, 10.3266);
 
   // PID controller for throuttle
-  pid_t.Init(0.02, 0.00002, 0.02);
+  pid_t.Init(2.9331, 0.0, 10.3266);
 
   h.onMessage([&pid_s, &pid_t](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
